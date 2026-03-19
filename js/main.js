bb85758f2824b907ee33d7cfcc5a37e981598cde
陳彥廷 <5b2g0030@stust.edu.tw>
2026-03-19 11:23:33 +0800

    更新

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 function goProjects() {
   window.location.href = "projects.html";
 }
+
+// 滾動動畫
+const faders = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+  faders.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if(top < window.innerHeight - 50){
+      el.classList.add('show');
+    }
+  });
+});
